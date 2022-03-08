0d550d32e247fd69795cfe2b05f76179ad6ec206
Juan Carlos Buitrago Almanza <juancarlos@tuorden.com.co>
2022-03-08 01:09:10 -0500

    ➕ Feat: Se agrega Paquete auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AuthModule } from '@auth0/auth0-angular';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +13,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    AuthModule.forRoot({
+      domain: 'dev-5crike50.us.auth0.com',
+      clientId: '4GiOl2RhkUTKhc3C3jsKwwBMohYqpizy',
+      cacheLocation: 'localstorage',
+      useRefreshTokens: true
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
